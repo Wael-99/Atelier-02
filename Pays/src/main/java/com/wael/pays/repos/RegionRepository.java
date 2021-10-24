c3deb82ca3000725d2c5258eb95e37ab7f29196c
wael benothmen <75394495+Wael-99@users.noreply.github.com>
2021-10-24 22:24:17 +0100

    Add files via upload

---
+++ Pays/src/main/java/com/wael/pays/repos/RegionRepository.java
@@ -0,0 +1,24 @@
+package com.wael.pays.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.wael.pays.entities.Region;
+import com.wael.pays.entities.pays;
+
+public interface RegionRepository extends JpaRepository<Region,Long>{
+	List<Region> findByNomRegion(String nom);
+	List<Region> findByNomRegionContains(String nom);
+	
+	@Query("select p from Region p where p.nomRegion like %?1 and p.surface > ?2")
+	List<Region> findBysurface (String nom, Double surface);
+	
+	@Query("select p from Region p where p.pays = ?1")
+	List<Region> findBypays(pays pays);
+	
+	@Query("select p from Region p order by p.nomRegion ASC, p.surface DESC")
+	List<Region> trierRegionsNomSurface ();
+	
+}
